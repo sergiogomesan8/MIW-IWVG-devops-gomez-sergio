911f6f32f3060a9c23021abc90780f6da66d2fb9
SergioGómez <sergio.gomesan8@gmail.com>
2022-10-07 01:08:32 +0200

    added Search 8 #10

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -0,0 +1,18 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchesTest {
+
+    @Test
+    void testFindUserFamilyNameBySomeImproperFraction() {
+        assertEquals(List.of("Fernandez", "Blanco", "López", "Torres", "Blanco", "Torres"),
+                new Searches().findUserFamilyNameBySomeImproperFraction().collect(Collectors.toList()));
+    }
+
+}
